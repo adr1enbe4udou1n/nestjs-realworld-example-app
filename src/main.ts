18123cf169cccf1d10a4964f779a6e071539714f
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-12-11 13:59:09 +0100

    use fastify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { EntityNotFoundFilter } from './filters/entity-not-found.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   app
     .useGlobalPipes(new ValidationPipe())
