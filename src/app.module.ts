08e88cf660f530ef9e352a677ca64b8b5045680e
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-05-05 22:15:01 +0200

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
-import { CoreModule } from './core.module';
 import { UsersModule } from './users/users.module';
 import { TagsModule } from './tags/tags.module';
 import { ArticlesModule } from './articles/articles.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { UserModule } from './user/user.module';
 import { MigrateService } from './migrate.service';
+import { ConfigModule } from '@nestjs/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
   imports: [
-    CoreModule,
+    ConfigModule.forRoot(),
+    MikroOrmModule.forRoot(),
     TagsModule,
     ArticlesModule,
     ProfilesModule,
