d0acea78174e915189616cebff29c2ac5922a068
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-05-08 18:58:42 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 import { EntityNotFoundFilter } from './filters/entity-not-found.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   app
     .useGlobalPipes(new ValidationPipe())
